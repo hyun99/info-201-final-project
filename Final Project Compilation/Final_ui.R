cd83485e0a8223f6cd94ca129428f441f7e24577
Sung Ahn <sahn1998@gmail.com>
2019-03-13 02:35:55 -0700

    added one description

--- Final Project Compilation/Final_ui.R
+++ Final Project Compilation/Final_ui.R
@@ -40,6 +40,21 @@ body <- dashboardBody(
   tabItems(
     tabItem(tabName = "Asthma",
             h2("Asthma Patients in California Counties from 2012 - 2016"),
+            p("Our group was curious in the number of Asthma
+              patients that had visited the hospital in Californian
+              counties, because over the past couple of decades, there
+              have been many wildfires within the state of California.
+              We hypothesized that there may be some correlation between
+              the number of asthma patients and the number of wildfires
+              that have occured during a certain year. Thus, we wanted to
+              see, from this data visualization shown below, how many
+              asthma patients were admitted from years 2012-2016 and
+              observe if there some years that were much higher than the other, or
+              see if there are visible trends occuring. We wanted to see exactly
+              how many asthma patients were in each respective county in order
+              to observe the correlation with asthma patients and wildfires. The
+              details of such as well as the details of wildfire severity can
+              be found on the next pages."),
      selectInput(
         "state1_choice",
         "Select 1st County to Observe",
@@ -82,6 +97,22 @@ body <- dashboardBody(
     tabItem(tabName = "WildFires",
             h2("Severity of WildFires in California Counties for Every 5 Years
                Since 2005"),
+            p("Continuing from the previous tab, "),
+            p("Our group was curious in the number of Asthma
+              patients that had visited the hospital in Californian
+              counties, because over the past couple of decades, there
+              have been many wildfires within the state of California.
+              We hypothesized that there may be some correlation between
+              the number of asthma patients and the number of wildfires
+              that have occured during a certain year. Thus, we wanted to
+              see, from this data visualization shown below, how many
+              asthma patients were admitted from years 2012-2016 and
+              is there some years that were much higher than the other, or
+              see if there is a visible trend occuring. We wanted to see exactly
+              how many asthma patients were in each respective county in order
+              to observe the correlation with asthma patients and wildfires. The
+              details of such as well as the details of wildfire severity can
+              be found on the next pages."),
             fluidRow(
               splitLayout(
                 cellWidths = c("40%", "40%", "40%"),
@@ -97,7 +128,7 @@ body <- dashboardBody(
 
 # CREATING MAIN UI
 final_ui <- dashboardPage(
-  dashboardHeader(title = "Asthma"),
+  dashboardHeader(title = "Asthma and California Wildfires"),
   sidebar,
   body
 )
