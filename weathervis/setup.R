b70187d4b75945c48599a16b6820ca4920ced418
Sung Ahn <sahn1998@gmail.com>
2019-03-13 23:23:24 -0700

    deleted necessary files

--- weathervis/setup.R
+++
@@ -1,9 +0,0 @@
-library(leaflet)
-library(lubridate)
-library(dplyr)
-library(RSQLite)
-
-# Importing and subsetting California Wildfire dataset
-ca_fire <- read.csv(file = "ca_fire.csv", stringsAsFactors = FALSE)
-fire <- ca_fire %>% 
-  select(FIRE_NAME, FIRE_SIZE, FIRE_SIZE_CLASS, STATE, COUNTY, "LOCATION" = SOURCE_REPORTING_UNIT_NAME)
